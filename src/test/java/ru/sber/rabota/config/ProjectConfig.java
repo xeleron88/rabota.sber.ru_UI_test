a023fea5a60f5732428681f540b04afd021011ab
Evgeniy <xelseo@gmail.com>
2022-11-17 17:15:10 +0300

    edit owner config

--- src/test/java/ru/sber/rabota/config/ProjectConfig.java
+++ src/test/java/ru/sber/rabota/config/ProjectConfig.java
@@ -6,29 +6,34 @@ import org.aeonbits.owner.Config;
 @Config.Sources({
         "system:properties",
         "classpath:${env}.properties",
-        "file:~/${env}.properties",
-        "file:./${env}.properties"
 })
 public interface ProjectConfig extends Config {
 
+    @Key("baseURL")
     @DefaultValue("https://rabota.sber.ru")
     String baseURL();
 
+    @Key("browser")
     @DefaultValue("chrome")
     String browser();
 
+    @Key("browserVersion")
     @DefaultValue("100.0")
     String browserVersion();
 
+    @Key("browserSize")
     @DefaultValue("1920x1080")
     String browserSize();
 
+    @Key("browserMobileView")
     @DefaultValue("")
     String browserMobileView();
 
+    @Key("remoteDriverUrl")
     @DefaultValue("")
     String remoteDriverUrl();
 
+    @Key("videoStorage")
     @DefaultValue("")
     String videoStorage();
 }
